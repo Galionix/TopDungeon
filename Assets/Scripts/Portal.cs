1230addfe4777d2bca7c8a3959b6b70a5d3f8dd5
dmitry <galionix2@gmail.com>
2023-02-21 19:53:11 +0200

    teleportation!

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : Collideable
+{
+
+    public string[] sceneNames;
+    // public Vector2 spawnPoint;
+    // public VectorValue playerStorage;
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.name == "Player")
+        {
+            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            SceneManager.LoadScene(sceneName);
+            // playerStorage.initialValue = spawnPoint;
+            // Debug.Log("Load level " + levelToLoad);
+        }
+    }
+}
