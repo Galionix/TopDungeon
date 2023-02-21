081937a14913df001542227515514aab25781114
dmitry <galionix2@gmail.com>
2023-02-21 19:38:56 +0200

    Commit message: Add Chest.cs, Collectable.cs, and Collideable.cs scripts with functionality for collection of objects.

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectable
+{
+    public Sprite emtpyChest;
+    public int pesosAmount = 5;
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (!collected)
+        {
+            base.OnCollect();
+            Debug.Log("Grant pesos " + pesosAmount);
+            GetComponent<SpriteRenderer>().sprite = emtpyChest;
+        }
+    }
+}
